4c09ec57c420e713147147a9aacc18b32e5d29c3
H2SMT <ykr6g3kx@gmail.com>
2021-11-26 09:51:19 +0000

    Initial Commit

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = '17ff788e6a44597b39409bf8c7dab6fec97a8503f134ef2ed497928367466fea68922546fe84b6f98a0e9b055b37aadf2d2ef359d1c14be204d42f89ba5eaccd'
